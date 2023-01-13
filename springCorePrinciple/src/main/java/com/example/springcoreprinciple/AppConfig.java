1f1d3c162c097ff38c28d86dadf65e0ea08a7772
J-Jaeh <1wo2gur@naver.com>
2023-01-13 17:36:31 +0900

    AppConfig 리펙토링 역할과 구현 분리

--- springCorePrinciple/src/main/java/com/example/springcoreprinciple/AppConfig.java
+++ springCorePrinciple/src/main/java/com/example/springcoreprinciple/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example.springcoreprinciple;
 
+import com.example.springcoreprinciple.discount.DiscountPolicy;
 import com.example.springcoreprinciple.discount.FixDiscountPolicy;
+import com.example.springcoreprinciple.member.MemberRepository;
 import com.example.springcoreprinciple.member.MemberServiceImple;
 import com.example.springcoreprinciple.member.Memberservice;
 import com.example.springcoreprinciple.member.MemoryMemberRepository;
@@ -10,10 +12,19 @@ import com.example.springcoreprinciple.order.OrderServiceImpl;
 public class AppConfig {
 
     public Memberservice memberservice() {
-        return new MemberServiceImple(new MemoryMemberRepository());  //여기서 구현체가 주입됨..!
+        return new MemberServiceImple(memberRepository());  //여기서 구현체가 주입됨..!
     }          // 나는 멤버서비스야 .! 멤버서비스 임플을 반환할껀데 new 메모리레포지트리를 가지고 반환할꺼야
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository() , new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), getDiscountPolicy());
     }
+
+    private static DiscountPolicy getDiscountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
 }
