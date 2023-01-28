ab6c45f79ce2d2990e1a3c998ef746769e1eb56c
J-Jaeh <1wo2gur@naver.com>
2023-01-28 18:25:28 +0900

    수동vs자동 충돌에 관한 이야기 ~

--- springCorePrinciple/src/main/java/com/example/springcoreprinciple/AutoAppConfig.java
+++ springCorePrinciple/src/main/java/com/example/springcoreprinciple/AutoAppConfig.java
@@ -1,6 +1,9 @@
 package com.example.springcoreprinciple;
 
 
+import com.example.springcoreprinciple.member.MemberRepository;
+import com.example.springcoreprinciple.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,4 +13,8 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig   {
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
