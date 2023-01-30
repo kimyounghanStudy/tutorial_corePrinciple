e1bcde6b04d352c3723c45e534444554c3119370
J-Jaeh <1wo2gur@naver.com>
2023-01-31 02:23:14 +0900

    인터페이스 이용 콜백

--- springCorePrinciple/src/test/java/com/example/springcoreprinciple/lifecycle/NetworkClient.java
+++ springCorePrinciple/src/test/java/com/example/springcoreprinciple/lifecycle/NetworkClient.java
@@ -1,14 +1,16 @@
 package com.example.springcoreprinciple.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean , DisposableBean {
 
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
     }
 
     public void seturl(String url) {
@@ -29,4 +31,14 @@ public class NetworkClient {
         System.out.println("close = " + url);
     }
 
+    @Override  //이름의미 ... 이존관계 주입이 끝나면 호출해 주겠다 ..
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+         disconnect();
+    }
 }
