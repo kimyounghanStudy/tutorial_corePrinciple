6c6d041c978a6e17fe475cada357432c25a0ca16
J-Jaeh <1wo2gur@naver.com>
2023-01-31 02:03:08 +0900

    빈라이프사이클..! 생성과 초기화를 분리

---
+++ springCorePrinciple/src/test/java/com/example/springcoreprinciple/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,33 @@
+package com.example.springcoreprinciple.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifecycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfiig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+
+
+    }
+
+    @Configuration
+    static class LifeCycleConfiig{
+
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.seturl("http://hello-spring.dev");
+
+            return networkClient;
+        }
+    }
+
+}
