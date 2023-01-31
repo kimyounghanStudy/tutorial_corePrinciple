5c4a96c3edc76c668202c83497f5f555a61bd918
J-Jaeh <1wo2gur@naver.com>
2023-01-31 19:31:11 +0900

    provide사용으로 호출시점을 지연시킴..!

--- springCorePrinciple/src/main/java/com/example/springcoreprinciple/web/LogDemoController.java
+++ springCorePrinciple/src/main/java/com/example/springcoreprinciple/web/LogDemoController.java
@@ -2,6 +2,7 @@ package com.example.springcoreprinciple.web;
 
 import com.example.springcoreprinciple.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody  // 문자를 그대로 응답보낼수 있다 ... 뷰안합치고 ..!
-    public String logDemo(HttpServletRequest request) /*H.S.R 고객 요청정보를 받을수있다.  */{
+    public String logDemo(HttpServletRequest request) /*H.S.R 고객 요청정보를 받을수있다.  */ {
+
         String requestURL = request.getRequestURI().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
--- springCorePrinciple/src/main/java/com/example/springcoreprinciple/web/LogDemoService.java
+++ springCorePrinciple/src/main/java/com/example/springcoreprinciple/web/LogDemoService.java
@@ -2,15 +2,18 @@ package com.example.springcoreprinciple.web;
 
 import com.example.springcoreprinciple.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
     public void logic(String id) {
 
-        myLogger.log("servic e id =" + id );
+        MyLogger myLogger = myLoggerProvider.getObject();
+
+        myLogger.log("service id =" + id );
     }
 }
